c643c464f7e5dd62472797dba7934b9ed6ace557
DESKTOP-L6LSGJ5\wangbo <bsw_is_u@163.com>
2016-10-15 11:24:33 +0800

    opencv cmake

---
+++ disp_avi/CMakeFiles/DisplayAvi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/DisplayAvi.dir/disp_avi.cpp.o"
+  "DisplayAvi.pdb"
+  "DisplayAvi"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/DisplayAvi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
